80f53e850b70e5a5385fe2b2de212ccc058f9312
Avnish Kumar <avnishkr.mbd@gmail.com>
2026-01-05 19:23:02 +0530

    jk

--- LMS/LMS/backend/routes/aiRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { searchWithAi } from "../controllers/aiController.js"
-
-let aiRouter = express.Router()
-
-aiRouter.post("/search",searchWithAi)
-
-export default aiRouter
