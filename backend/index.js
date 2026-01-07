6c94bf16a66cab6bc59acd589ebed15229319a56
Avnish Kumar <avnishkr.mbd@gmail.com>
2026-01-07 13:48:23 +0530

    Added some important configuration

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5174","http://localhost:5173","http://localhost:3000"],
+    origin:["http://localhost:5174","http://localhost:5173","http://localhost:3000","https://edu-flex0.vercel.app"],
     credentials:true
 }))
 app.use("/api/auth", authRouter)
